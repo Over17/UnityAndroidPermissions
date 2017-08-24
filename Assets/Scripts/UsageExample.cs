9bfa91c714bbfeac228c85a9fdc57c6c5027e1c3
Yury Habets <over17@outlook.com>
2017-08-24 11:59:23 +0200

    Updated comments, some minor changes in the sample script

--- Assets/Scripts/UsageExample.cs
+++ Assets/Scripts/UsageExample.cs
@@ -1,25 +1,24 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class UsageExample : MonoBehaviour {
-
+public class UsageExample : MonoBehaviour
+{
     private const string STORAGE_PERMISSION = "android.permission.READ_EXTERNAL_STORAGE";
 
-
-    //Function which called first, by UI button, for example click on Avatar to change it from device gallery
+    // Function to be called first (by UI button)
+    // For example, click on Avatar to change it from the device gallery
     public void OnBrowseGalleryButtonPress()
     {
         if (!CheckPermissions())
         {
-            Debug.Log("Missing permission to browse device gallery, please grant permission first");
+            Debug.LogWarning("Missing permission to browse device gallery, please grant the permission first");
 
-            //Your code to show in-game pop-up with explanation why do you need this permission (required for Google Featuring program)
-            //this pop-up should include a button "Grant Access" which will be linked to the function "OnGrantButtonPress" below
+            // Your code to show in-game pop-up with the explanation why you need this permission (required for Google Featuring program)
+            // This pop-up should include a button "Grant Access" linked to the function "OnGrantButtonPress" below
             return;
         }
 
-        //Your code to browse Android Gallery
+        // Your code to browse Android Gallery
         Debug.Log("Browsing Gallery...");
     }
 
@@ -38,13 +37,14 @@ public class UsageExample : MonoBehaviour {
         AndroidPermissionsManager.RequestPermission(new []{STORAGE_PERMISSION}, new AndroidPermissionCallback(
             grantedPermission =>
             {
-                //Permission was successfully granted, restart the change avatar routine
+                // The permission was successfully granted, restart the change avatar routine
                 OnBrowseGalleryButtonPress();
             },
             deniedPermission =>
             {
-                //Permission was not granted
-                //Show in-game pop-up message stating that user can change permissions in Android Application Settings if he would change his mind (Also required by Google Featuring programm)
+                // The permission was denied.
+                // Show in-game pop-up message stating that the user can change permissions in Android Application Settings
+                // if he changes his mind (also required by Google Featuring program)
             }));
     }
 }
